b7594fbd670816e0a3980bf0d45819a5e219e80a
elinus <sunilmourya.cse.nitp@gmail.com>
2022-03-01 08:49:28 +0530

    update readme

---
+++ java/Two_Sum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class Two_Sum {
+    
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
+        int len = nums.length;
+        for(int i = 0; i < len; i++){
+            if(tracker.containsKey(nums[i])){
+                int left = tracker.get(nums[i]);
+                return new int[]{left+1, i+1};
+            }else{
+                tracker.put(target - nums[i], i);
+            }
+        }
+        return new int[2];
+    }
+
+    public static void main(String [] args) {
+        
+    }
+}
