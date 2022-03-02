86b80fadd9fc9fc794b24c719472c6824057bf23
elinus <sunilmourya.cse.nitp@gmail.com>
2022-03-02 07:34:04 +0530

    Start with - Programming Skills I

---
+++ cpp/1491_average_salary_excluding_the_minimum_and_maximum_salary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int min_ = INT_MAX, max_ = INT_MIN;
+        double sum = 0;
+        for (int i = 0; i < salary.size(); i++)
+        {
+            sum += salary[i];
+            if (salary[i] < min_) min_ = salary[i];
+            if (salary[i] > max_) max_ = salary[i];
+        }
+        sum -= (min_ + max_);
+        return sum / (salary.size() - 2);
+    }
+};
---
+++ cpp/1523_count_odd_numbers_in_an_interval_range.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int diff = high - low;
+        diff /= 2;
+        if (low % 2 || high % 2) diff +=1;
+        return diff;
+    }
+};
