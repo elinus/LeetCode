10421a0a09617e862f27469a4e30f1a13ca553c2
Sunil Mourya (elinus) <sunilmourya.cse.nitp@gmail.com>
2024-09-17 22:36:07 +0530

    Tree Traversal - In/Pre/Post
    94. Binary Tree Inorder Traversal
    LeetCode Submission URL(Cpp): https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/1393456203/
    144. Binary Tree Preorder Traversal
    LeetCode Submission URL(Cpp): https://leetcode.com/problems/binary-tree-preorder-traversal/submissions/1393463704/
    145. Binary Tree Postorder Traversal
    LeetCode Submission URL(Cpp): https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/1393470553/

---
+++ problems/trees/cpp/144_binary_tree_preorder_traversal.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+// Definition of TreeNode for the binary tree
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  /**
+   * @complexity
+   * - Time: O(n), where n is the number of nodes in the tree. Each
+   * node is visited once.
+   * - Space: O(h), where h is the height of the tree. In the worst
+   * case (unbalanced tree), it can be O(n).
+   */
+  vector<int> preorderTraversal(TreeNode *root) {
+    vector<int> result;           // Stores the in-order traversal result
+    stack<TreeNode *> node_stack; // Stack to hold nodes as we traverse
+
+    // Continue until all nodes are processed
+    while (!node_stack.empty() || root != nullptr) {
+      // Traverse to the leftmost node
+      while (root != nullptr) {
+        result.push_back(root->val); // Add the node's value to the result
+        node_stack.push(root);
+        root = root->left;
+      }
+
+      // Process the node at the top of the stack
+      root = node_stack.top();
+      node_stack.pop();
+
+      // Move to the right subtree
+      root = root->right;
+    }
+
+    return result;
+  }
+};
+
+int main() {
+  // code
+  return 0;
+}
